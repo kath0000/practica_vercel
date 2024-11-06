23b6fa15f4771a754898a0fc8ee4274cbbf96bed
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:30:22 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "ELECCIONES EE.UU",
+"autor" => "Trump, presidente por segunda vez",
+"resumen" => "El republicano se impone con claridad a Kamala Harris y volverá a la Casa Blanca cuatro años después de su tormentosa salida",
 ];
 ?>
