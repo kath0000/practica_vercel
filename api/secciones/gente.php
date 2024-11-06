e5cb200c6130354e9ce1b8d1ab45265e6dce5b7f
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:46:38 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Marazzis",
+"autor" => "Laura Fa",
+"resumen" => "La castastrofe en Valencia",
 ];
 ?>
