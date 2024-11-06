9be90682e4ff53ba43f46046f35abf52a5b09bbf
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:48:48 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenShift RedHat",
+"autor" => "@RedHat",
+"resumen" => "Es una plataforma confiable, integral y consistente para desarrollar, modernizar e implementar aplicaciones a escala",
 ];
 ?>
