b692adf8bc22a7e4113f1e2e8a2f00f89f59f0ae
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:45:13 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "Educación para Vivir",
+"autor" => "Eugene Hoshiko",
+"resumen" => "Los lectores escriben sobre el civismo de los japoneses, las reacciones en las redes sociales a la muerte de Itziar Castro",
 ];
 ?>
