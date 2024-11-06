5b72d888496f57c39f3b1a287c28756d8bccd7f0
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:39:22 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Política Nacional de Cultura del Perú",
+"autor" => "Ministerio de Cultura",
+"resumen" => "El Poder Ejecutivo aprobó la primera Política Nacional de Cultura en la historia del Perú, que orientará e integrará durante los próximos diez años.",
 ];
 ?>
