c994052f007456cb4b9061d3a9b2b200e6cce72e
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:37:53 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "TEKNOPOLIS",
+"autor" => "eit.bus",
+"resumen" => "El programa ''Teknopolis'' es una ventana abierta a la ciencia, a la tecnología y a la innovación",
 ];
 ?>
