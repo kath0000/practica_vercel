636881ba43e2d093a6ca25b487c86eb6c2c1e6f1
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:34:07 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "PRECIO DEL DOLAR EN PEÚ",
+"autor" => "El Comercio",
+"resumen" => "El precio del dólar en el país cerró el 5 de noviembre de 2024 en S/3,7740",
 ];
 ?>
