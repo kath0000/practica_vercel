32a674056af7ccdf7ed4dd77c371c817ec0bb57f
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:41:14 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Escasez mundial de contenedores ",
+"autor" => "Javier Artica",
+"resumen" => "Se cuadriplica las tarifas de fletes marítimos en el Perú",
 ];
 ?>
