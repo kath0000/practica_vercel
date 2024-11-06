23ed563e3dd7721e79c9e74e32e8d6fc70576a71
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:42:59 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "CONVOCATORIA DE EMPLEO 2024",
+"autor" => "La República",
+"resumen" => "Municipalidad lanza 79 ofertas de trabajo hasta el 12 de noviembre",
 ];
 ?>
