4f8d0861666684ac27d8ad4dcf18a8ab80d88695
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:35:50 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Menjunje mental en Palacio",
+"autor" => "Giulio Valz-Gen",
+"resumen" => "La presidenta Dina Boluarte anda cada vez más alejada y desconectada de la realidad, eso es lo que se desprende de la interpretación de sus decisiones y declaraciones públicas.",
 ];
 ?>
