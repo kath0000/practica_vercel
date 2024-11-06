69a2477115a4047f7ed177fe7f9ec2354361b8e3
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:32:02 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "NOTICIA PERÚ",
+"autor" => "CNN",
+"resumen" => "Un futbolista muerto y 4 heridos tras impacto de rayo durante juego en Perú",
 ];
 ?>
