b5c009ef8d98698052e05c0d60f3e7746b586a8d
kath0000 <kathiahuamanitavara@gmail.com>
2024-11-06 11:52:54 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "ATV",
+"autor" => "atv.pe",
+"resumen" => "7:00 pm - 8:00 pm Noticiero",
 ];
 ?>
